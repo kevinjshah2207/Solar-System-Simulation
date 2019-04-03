6e4381b0d5a5334a03f1ab25277e16355ef0de89
maulikchevli <maulikchevli@gmail.com>
2019-04-03 15:32:11 +0530

    Add rotation to Sun

--- solar/Solar.gd
+++ solar/Solar.gd
@@ -1,9 +1,20 @@
 extends Spatial
 
 func _ready():
+	print($Sun.translation - $Earth.translation)
 	pass # Replace with function body.
-var angle = 0.15
-func _process(delta):
-	$Earth.transform.basis = $Earth.transform.basis.rotated(Vector3(0, 0, 1), angle)
-	angle += delta
 
+var radius=0.5
+var x=0;
+var y=0;
+var angle=0.0
+func _process(delta):
+	$Earth.global_translate(Vector3(x,0,y))
+	
+	#print(y)
+	#print(x)
+	x = radius*cos(angle)
+	y = radius*sin(angle)
+	
+	#print("angle :" + str(angle))
+	angle = (angle+0.01)
