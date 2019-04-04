5a57c86802b9d5464c0e198b4f867870708688ec
maulikchevli <maulikchevli@gmail.com>
2019-04-04 09:22:10 +0530

    Add keyboard events

--- solar/Solar.gd
+++ solar/Solar.gd
@@ -4,13 +4,30 @@ func _ready():
 	#print($Sun.translation - $Earth.translation)
 	pass # Replace with function body.
 
-var radius=0.5
-var x=0;
-var y=0;
-var angle=0.005
+
+var speed=0.005
+var moon_speed = 0.05
+
 func _process(delta):
-	$Sun.rotate_y(angle)
-	$Sun/Earth.rotate_x(angle)
+	$Sun/Earth.rotate_x(moon_speed)
+	$Sun.rotate_y(speed)
 	
-	if angle > PI:
-		angle = 0
+	
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and Input.is_action_pressed("ui_right") and event.scancode == KEY_R:
+			$Camera.rotate_z(0.01)
+		elif Input.is_action_pressed("ui_up"):
+			$Camera.translate(Vector3(0, 0.1, 0))
+		elif Input.is_action_pressed("ui_down"):
+			$Camera.translate(Vector3(0, -0.1, 0))
+		elif Input.is_action_pressed("ui_right"):
+			$Camera.translate(Vector3(0.1, 0, 0))
+		elif Input.is_action_pressed("ui_left"):
+			$Camera.translate(Vector3(-0.1, 0, 0))
+		elif Input.is_action_pressed("ui_page_up"):
+			$Camera.translate(Vector3(0, 0, 0.1))
+		elif Input.is_action_pressed("ui_page_down"):
+			$Camera.translate(Vector3(0, 0, -0.1))
+			
